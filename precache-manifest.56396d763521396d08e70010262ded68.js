bdb6694f9806a6f66e5b9dc219890d843bdf836f
unknown <shaonkabir98@gmail.com>
2019-07-29 09:16:16 -0700

    Updates

---
+++ precache-manifest.56396d763521396d08e70010262ded68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c6d79492f269b575bb70fb429f3deb",
+    "url": "/photoShowCase/index.html"
+  },
+  {
+    "revision": "f8f5105ea8e188333807",
+    "url": "/photoShowCase/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "d2979e0d1d2d313baf31",
+    "url": "/photoShowCase/static/css/main.cbe78828.chunk.css"
+  },
+  {
+    "revision": "f8f5105ea8e188333807",
+    "url": "/photoShowCase/static/js/2.d84687f8.chunk.js"
+  },
+  {
+    "revision": "d2979e0d1d2d313baf31",
+    "url": "/photoShowCase/static/js/main.0dad96ba.chunk.js"
+  },
+  {
+    "revision": "2134d1f47f581dbd4285",
+    "url": "/photoShowCase/static/js/runtime~main.ca544589.js"
+  },
+  {
+    "revision": "ebab618fba1acece588d6237437783f5",
+    "url": "/photoShowCase/static/media/rasel-vai.ebab618f.jpg"
+  }
+]);
